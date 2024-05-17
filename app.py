4790950722e6d54f58294b87f3e831f43bb8e47b
sshaw00 <swarupk102001@gmail.com>
2024-05-17 12:34:23 +0530

    NewCommit

---
+++ app.py
@@ -0,0 +1,33 @@
+import openai
+import gradio as gr
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+print(gr.__version__)
+
+openai.api_key = os.getenv("API")
+
+# Initialize messages within the function to avoid retaining state between calls
+def CustomChatGPT(user_input, messages=None):
+    if messages is None:
+        messages = [{"role": "system", "content": "Creative teacher specialising in Design Thinking on Project Based Learning"}]
+    
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+# Use gradio instead of gr
+demo = gr.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Real Estate Pro")
+
+# Get the port from the environment variable, or default to 7861
+port = int(os.getenv("PORT", 7861))
+
+# Specify the host and port for the Gradio app
+demo.launch(share=True, server_port=port)
